972256a715382f833f170dd0ff04026a19db4214
Thebks <ichbks@gmail.com>
2023-06-06 22:46:04 +0500

    wip home

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const useMediaQuery = (query) => {
-    const { matches, setMatches } = useState(false);
+    const [matches, setMatches] = useState(false);
 
     useEffect(() => {
         const media = window.matchMedia(query);
