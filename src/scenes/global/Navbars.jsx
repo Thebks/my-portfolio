d1cee23307edeae91756ae121e6757bd1331a24e
Thebks <ichbks@gmail.com>
2023-06-10 22:27:16 +0500

    media

--- src/scenes/global/Navbars.jsx
+++ src/scenes/global/Navbars.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import useMediaQuery from "../../hooks/useMediaQuery"
-import { AppBar, Box, IconButton, Toolbar, Typography, makeStyles, useTheme } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, makeStyles, useTheme } from '@mui/material';
 import Button from '../../components/Button';
 import { Link, NavLink } from 'react-router-dom'
 import { setMenuActive } from './menuActiveLinkSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import MenuIcon from '@mui/icons-material/Menu'
+import IconButton from '@mui/material/IconButton';
 
 const Navbars = () => {
 
@@ -16,16 +17,13 @@ const Navbars = () => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreen = useMediaQuery("(min-width:768px)");
 
-    const handleButtonClick = () => console.log("you clicked me");
 
+    const handleButtonClick = () => console.log("you clicked me");
 
     const handleClickRedux = (to) => {
         dispatch(setMenuActive(to));
     }
 
-    const handleMenuToggle = () => {
-        setIsMenuToggled(!isMenuToggled);
-    };
 
     return (
         <AppBar sx={{ zIndex: 40, position: 'static', top: 0, height: '80px', marginTop: '50px', borderBottom: '2px solid black', borderTop: '2px solid black', }}>
@@ -152,21 +150,139 @@ const Navbars = () => {
                         </Box>
                     </Box>
                 ) : (
-                    <Box>
-                        <IconButton edge='start' color="inherit" aria-label="menu" sx={{ marginLeft: '8px' }}>
-                            <MenuIcon onClick={isMenuToggled ? handleMenuToggle : null} />
+                    <IconButton edge='start' color="inherit" aria-label="menu" sx={{ marginLeft: '8px', }}
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                )}
+
+                {!isAboveSmallScreen && isMenuToggled && (
+                    <Box sx={{ position: 'fixed', bottom: 0, left: 0, height: '100%', backgroundColor: palette.primary[500], width: '300px' }}>
+                        <IconButton sx={{ display: 'flex', justifyContent: 'flex-end', p: '12px' }}
+                            onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        >
+                            <MenuIcon />
                         </IconButton>
-                        {isAboveSmallScreen && isMenuToggled && (
-                            <Box sx={{ position: 'fixed', right: 0, bottom: 0, height: '100%', backgroundColor: palette.green[500], width: '300px' }}>
-                                <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 12 }}>
-                                    <MenuIcon onClick={handleMenuToggle} />
-                                </Box>
 
-                                {/* Menu Items */}
+                        {/* Menu Items */}
+                        {/* Home */}
+                        <Box
+                            component={Link}
+                            to="/"
+                            sx={{
+                                color: 'inherit',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textDecoration: 'none',
+                                boxSizing: 'border-box',
+                                padding: '16px 20px',
+                                width: '80px',
+                                height: '49px',
+                                borderRadius: activeLinkCircle === '/' ? '30px' : '0',
+                                border: activeLinkCircle === '/' ? `1px solid ${palette.black[500]}` : 'none',
+                                backgroundColor: activeLinkCircle === '/' ? '#FFFFFF' : 'inherit',
+                                '&:hover': {
+                                    border: '3px solid #000000',
+                                    boxShadow: '3px 4px 0px 2px #000000',
+                                    borderRadius: '30px',
+                                    background: '#FFFFFF',
+                                    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                                },
+                            }}
+                            onClick={() => handleClickRedux('/')}
+                        >
+                            Home
+                        </Box>
+                        {/* features */}
+                        <Box
+                            component={Link}
+                            to="/features"
+                            sx={{
+                                color: 'inherit',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textDecoration: 'none',
+                                boxSizing: 'border-box',
+                                padding: '16px 20px',
+                                width: '80px',
+                                height: '49px',
+                                borderRadius: activeLinkCircle === '/features' ? '30px' : '0',
+                                border: activeLinkCircle === '/features' ? `1px solid ${palette.black[500]}` : 'none',
+                                backgroundColor: activeLinkCircle === '/features' ? '#FFFFFF' : 'inherit',
+                                '&:hover': {
+                                    border: '3px solid #000000',
+                                    boxShadow: '3px 4px 0px 2px #000000',
+                                    borderRadius: '30px',
+                                    background: '#FFFFFF',
+                                    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                                },
+                            }}
+                            onClick={() => handleClickRedux('/features')}
+                        >
+                            Features
+                        </Box>
 
-
-                            </Box>
-                        )}
+                        {/* Contact */}
+                        <Box
+                            component={Link}
+                            to="/contact"
+                            sx={{
+                                color: 'inherit',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textDecoration: 'none',
+                                boxSizing: 'border-box',
+                                padding: '16px 20px',
+                                width: '80px',
+                                height: '49px',
+                                borderRadius: activeLinkCircle === '/contact' ? '30px' : '0',
+                                border: activeLinkCircle === '/contact' ? `1px solid ${palette.black[500]}` : 'none',
+                                backgroundColor: activeLinkCircle === '/contact' ? '#FFFFFF' : 'inherit',
+                                '&:hover': {
+                                    border: '3px solid #000000',
+                                    boxShadow: '3px 4px 0px 2px #000000',
+                                    borderRadius: '30px',
+                                    background: '#FFFFFF',
+                                    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                                },
+                            }}
+                            onClick={() => handleClickRedux('/contact')}
+                        >
+                            Contact
+                        </Box>
+                        {/* Projects */}
+                        <Box
+                            component={Link}
+                            to="/projects"
+                            sx={{
+                                color: 'inherit',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textDecoration: 'none',
+                                boxSizing: 'border-box',
+                                padding: '16px 20px',
+                                width: '80px',
+                                height: '49px',
+                                borderRadius: activeLinkCircle === '/projects' ? '30px' : '0',
+                                border: activeLinkCircle === '/projects' ? `1px solid ${palette.black[500]}` : 'none',
+                                backgroundColor: activeLinkCircle === '/projects' ? '#FFFFFF' : 'inherit',
+                                '&:hover': {
+                                    border: '3px solid #000000',
+                                    boxShadow: '3px 4px 0px 2px #000000',
+                                    borderRadius: '30px',
+                                    background: '#FFFFFF',
+                                    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                                },
+                            }}
+                            onClick={() => handleClickRedux('/projects')}
+                        >
+                            Projects
+                        </Box>
                     </Box>
                 )}
 
