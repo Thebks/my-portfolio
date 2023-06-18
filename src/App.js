1603f6f06554be9e223295b673d00623b795d7f8
Thebks <ichbks@gmail.com>
2023-06-18 23:02:51 +0500

    dotGroup

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { createTheme } from '@mui/material/styles';
 import { themeSettings } from "./theme";
 import Navbar from "./scenes/global/Navbar"
 // import Navbars from "./scenes/global/Navbars"
+import DotGroup from "./scenes/global/DotGroup"
 import Home from "./scenes/home";
 import Features from "./scenes/features";
+import { Box } from "@mui/material";
+import useMediaQuery from "./hooks/useMediaQuery"
+
 
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
   return (
     <div className="app">
@@ -18,6 +23,11 @@ const App = () => {
         <CssBaseline />
         <ThemeProvider theme={theme}>
           <main className="content">
+            {/* <Box sx={{ width: '5/6', marginX: 'auto', height: '100%', height: { xs: "100%", md: "100vh" }, }}>
+              {isDesktop && (
+              )}
+            </Box> */}
+            <DotGroup />
             {/* <Navbars /> */}
             <Navbar />
             <Routes>
--- src/scenes/global/DotGroup.jsx
+++ src/scenes/global/DotGroup.jsx
@@ -1,40 +1,98 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import useMediaQuery from "../../hooks/useMediaQuery"
+import { setMenuActive, toggleMenu } from './menuActiveLinkSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, useTheme } from '@mui/material';
+import { Link } from "react-router-dom"
 
+const DotGroup = () => {
 
-const useStyles = makeStyles((theme) => ({
-    selectedStyles: {
+    const dispatch = useDispatch();
+    const isDesktop = useMediaQuery("(min-width: 1060px)");
+    const { menuActive, menuToggled } = useSelector((state) => state.menuActiveLink);
+    const { palette } = useTheme();
+
+    const selectedStyles = {
         position: 'relative',
-        backgroundColor: 'yellow',
+        backgroundColor: palette.secondary[500],
         '&::before': {
-            content: "''",
+            content: '""',
             position: 'absolute',
             width: '6px',
             height: '6px',
             borderRadius: '50%',
             borderWidth: '2px',
-            borderColor: 'yellow',
+            borderColor: palette.secondary[500],
             left: '-50%',
             top: '-50%',
         },
-    },
-}));
+    };
 
-const DotGroup = () => {
     return (
-        <Box sx={{ display: 'flex', felxDirection: 'column', gap: '1.5rem', position: 'fixed', top: '60%', right: '7px' }}>
-            <Link
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', position: 'fixed', top: '60%', right: '7px' }}>
+            <Box
                 to="/"
-                component={Box}
+                component={Link}
                 sx={{
-                    width: "3px",
-                    height: "3px",
-                    borderRadius: "50%",
-                    backgroundColor: location.pathname === "/" ? "yellow" : "darkgrey",
+                    ...((menuActive === "/") ? selectedStyles : { backgroundColor: palette.green[500] }),
+                    width: 10,
+                    height: 10,
+                    borderRadius: '50%',
                 }}
+                onClick={() => dispatch(setMenuActive("/"))}
+                smooth={true}
+            />
+
+            {/*FEATURES*/}
+            <Box
+                to="/features"
+                component={Link}
+                sx={{
+                    ...((menuActive === "/features") ? selectedStyles : { backgroundColor: palette.green[500] }),
+                    width: '10px',
+                    height: '10px',
+                    borderRadius: '50%',
+                }}
+                onClick={() => dispatch(setMenuActive("/features"))}
+                smooth={true}
+            />
+
+            {/* CONTACTS */}
+            <Box
+                to="/contact"
+                component={Link}
+                sx={{
+                    ...((menuActive === "/contact") ? selectedStyles : { backgroundColor: palette.green[500] }),
+                    width: '10px',
+                    height: '10px',
+                    borderRadius: '50%',
+                }}
+                onClick={() => dispatch(setMenuActive("/contact"))}
+                smooth={true}
+            />
+            {/* PROJECTS */}
+            <Box
+                to="/projects"
+                component={Link}
+                sx={{
+                    ...((menuActive === "/projects") ? selectedStyles : { backgroundColor: palette.green[500] }),
+                    width: '10px',
+                    height: '10px',
+                    borderRadius: '50%',
+                }}
+                onClick={() => dispatch(setMenuActive("/projects"))}
+                smooth={true}
             />
         </Box>
     )
 }
 
 export default DotGroup
+
+// import DotGroup from "./scenes/global/DotGroup"
+{/* <Box sx={{ width: '5/6', marginX: 'auto', height: '100%', height: { xs: "100%", md: "100vh" }, }}>
+    {isDesktop &&(
+        <DotGroup/>
+    )}
+</Box> */}
