9ce320c7fd37ae7a16132c076ababa0eff0eb948
Thebks <ichbks@gmail.com>
2023-07-03 22:35:21 +0500

    wip home

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -25,39 +25,67 @@ function Home() {
                         <>
                             {/* Image Section */}
                             <Grid xs={12} md={6} item order={isAboveLargeScreen ? 1 : 0}>
+
                                 <Box sx={{
                                     position: 'relative', ml: 2,
                                     '&::before': {
                                         content: '""',
                                         position: 'absolute',
-                                        top: '-10px',
-                                        left: '320px',
+                                        top: '-12px',
+                                        left: '365px',
                                         zIndex: 0,
-                                        width: '420px',
-                                        maxWidth: '400px',
-                                        height: '575px',
-                                        border: '2px solid',
-                                        borderColor: palette.primary[500],
-                                        borderRadius: '120px 30px 30px 30px'
+                                        width: '100%',
+                                        maxWidth: '363px',
+                                        height: '510px',
+                                        border: '3px solid black',
+                                        borderRadius: '150px 30px 30px 30px ',
+                                        background: palette.green[500],
+                                        boxShadow: '10px 10px 0px rgba(0, 0, 0, 1)',
+                                        overflow: 'hidden'
                                     }
                                 }}>
-                                    <Image
+                                    <Box component="img"
                                         alt='ppp'
                                         src={require('../../assets/cryptoDog.jpeg')}
                                         sx={{
+                                            position: 'relative',
                                             top: '-8px',
-                                            left: '55px',
+                                            left: '370px',
+                                            objectFit: 'cover',
                                             width: '100%',
-                                            maxWidth: '400px',
+                                            maxWidth: '350px',
                                             transition: 'filter 0.5s ease-in-out',
-                                            borderRadius: '120px 30px 30px 30px',
+                                            borderRadius: '150px 30px 30px 30px',
+                                            border: '3px solid',
+                                            '&:hover': {
+                                                filter: 'saturate(200%)',
+                                                transitionDuration: '500ms',
+                                                transform: 'scale(1.01)',
+                                            },
+
+                                        }}
+                                    />
+                                    {/* <Box
+                                        component="img"
+                                        alt='ppp'
+                                        src={require('../../assets/cryptoDog.jpeg')}
+                                        sx={{
+                                            position: 'relative',
+                                            top: '50%',
+                                            left: '50%',
+                                            objectFit: 'cover',
+                                            width: '100%',
+                                            maxWidth: '360px',
+                                            transition: 'filter 0.5s ease-in-out',
+                                            borderRadius: '150px 30px 30px 30px',
+                                            border: '3px solid',
                                             '&:hover': {
                                                 filter: 'saturate(200%)',
                                                 transitionDuration: '500ms',
                                                 transform: 'scale(1.01)',
                                             },
                                         }}
-                                    />
+                                    /> */}
                                 </Box>
                             </Grid>
                         </>
@@ -80,56 +108,59 @@ function Home() {
                     <Grid item xs={12} md={6} order={isAboveLargeScreen ? 0 : 1}>
                         <Box sx={{ ml: { md: 2 } }}>
                             {/* HEADINGS */}
-                            <motion.div
-                                initial="hidden"
-                                whileInView="visible"
-                                viewport={{ once: true, amount: 0.5 }}
-                                transition={{ duration: 0.5 }}
-                                variants={{
-                                    hidden: { opacity: 0, x: -50 },
-                                    visible: { opacity: 1, x: 0 },
-                                }}>
-                                <Typography
-                                    variant="h3"
-                                    component="p"
-                                    align="center"
-                                    sx={{
-                                        fontSize: '6xl',
-                                        fontFamily: 'Playfair Display, serif',
-                                        position: 'relative',
-                                        zIndex: 10,
-                                        '& span': {
+                            <Box>
+
+                                <motion.div
+                                    initial="hidden"
+                                    whileInView="visible"
+                                    viewport={{ once: true, amount: 0.5 }}
+                                    transition={{ duration: 0.5 }}
+                                    variants={{
+                                        hidden: { opacity: 0, x: -50 },
+                                        visible: { opacity: 1, x: 0 },
+                                    }}>
+                                    <Typography
+                                        variant="h3"
+                                        component="p"
+                                        align="center"
+                                        sx={{
+                                            fontSize: '6xl',
+                                            fontFamily: 'Playfair Display, serif',
                                             position: 'relative',
-                                            zIndex: 20,
-                                            color: palette.primary[500],
-                                            fontWeight: 'bold',
-                                            '&::before': {
-                                                content: '""',
-                                                position: 'absolute',
-                                                left: '-25px',
-                                                top: '-70px',
-                                                zIndex: -1,
-                                                color: 'brush',
-                                                borderRadius: '0',
+                                            zIndex: 10,
+                                            '& span': {
+                                                position: 'relative',
+                                                zIndex: 20,
+                                                color: palette.primary[500],
+                                                fontWeight: 'bold',
+                                                '&::before': {
+                                                    content: '""',
+                                                    position: 'absolute',
+                                                    left: '-25px',
+                                                    top: '-70px',
+                                                    zIndex: -1,
+                                                    color: 'brush',
+                                                    borderRadius: '0',
+                                                },
                                             },
-                                        },
-                                    }}>The <span>Big</span>  Hello</Typography>
-                                <Typography
-                                    variant="body1"
-                                    align="center"
-                                    sx={{
-                                        mt: 2,
-                                        fontSize: 'sm',
-                                    }}
-                                >
-                                    Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
-                                </Typography>
-                            </motion.div>
+                                        }}>The <span>Big</span>  Hello</Typography>
+                                    <Typography
+                                        variant="body1"
+                                        align="center"
+                                        sx={{
+                                            mt: 2,
+                                            fontSize: 'sm',
+                                        }}
+                                    >
+                                        Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+                                    </Typography>
+                                </motion.div>
+                            </Box>
                         </Box>
                     </Grid>
                 </>
             </Grid>
-        </Box>
+        </Box >
     );
 }
 
